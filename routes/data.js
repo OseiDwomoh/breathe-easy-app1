ee15d4f45ea6454d30e1603828afabf53a11341e
OseiDwomoh <164889073+OseiDwomoh@users.noreply.github.com>
2024-06-07 09:48:29 -0400

    Update data.js

--- routes/data.js
+++ routes/data.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 
 const router = express.Router();
 
-//comment on what this code (lines 7-13) does
+//Administration
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -12,7 +12,7 @@ const pool = new Pool({
     port: process.env.DB_PORT
 });
 
-// Route to fetch all observation data
+// Error Info
 router.get('/data', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM observations');
